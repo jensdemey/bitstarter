9071fdaedafeca205104cbfd878aa68118dba982
Jens De Mey <jens.demey@telenet.be>
2013-08-15 19:44:02 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! Is everybody HAPPY???');
+  response.send(buf.toString(fs.readFile(index.html)));
 });
 
 var port = process.env.PORT || 5000;
