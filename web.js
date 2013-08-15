0a879cacbe30cff2da70f1b287866548e4190996
Jens De Mey <jens.demey@telenet.be>
2013-08-15 20:12:57 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-//buf = new Buffer(50);
-//buf = fs.readFileSync(index.html);
+buf = new Buffer(50);
+buf = fs.readFileSync(index.html);
 app.get('/', function(request, response) {
-  response.send("Hi");
+  response.send("Hi2");
 });
 
 var port = process.env.PORT || 5000;
