95ff7aafeb80248dcc85e43ba0eab2f615b0c441
Jens De Mey <jens.demey@telenet.be>
2013-08-15 20:22:15 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 var buf = new Buffer(50);
-buf = fs.readFileSync(index.html);
+//buf = fs.readFileSync(index.html);
 app.get('/', function(request, response) {
   response.send("Hi2");
 });
